717f4f01305ae143bf75fe9500018ee14a902ec7
Swati108 <swatidec2003@gmail.com>
2025-09-21 18:29:13 +0530

    .\NestedTryCatch.java

---
+++ Day4_Exception/NestedTryCatch.java
@@ -0,0 +1,22 @@
+package Day4_Exception;
+
+import java.util.Scanner;
+public class NestedTryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {10, 20, 30, 40, 50};
+        int index = sc.nextInt();
+        int divisor = sc.nextInt();
+        try {
+            int value = arr[index];
+            try {
+                int result = value / divisor;
+                System.out.println(result);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        }
+    }
+}
